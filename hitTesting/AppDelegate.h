d22e8ed44e459864fb9d2549a7426c617a94afe9
tabedoc <codebat@126.com>
2016-03-01 14:58:28 +0800

    Initial Commit

---
+++ hitTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hitTesting
+//
+//  Created by liuwp on 16/3/1.
+//  Copyright © 2016年 liuwp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hitTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hitTesting
+//
+//  Created by liuwp on 16/3/1.
+//  Copyright © 2016年 liuwp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
